0673f90454ba96f7e70f19c600f59c816c2579aa
sigmasternchen <git@sigma-star.io>
2024-10-29 22:08:24 +0100

    the paddle can move now

--- src/game/Paddle.ts
+++ src/game/Paddle.ts
@@ -1,3 +1,4 @@
+import {fieldWidth} from "./geometry";
 
 export class Paddle {
     private position: number;
@@ -5,16 +6,33 @@ export class Paddle {
     private readonly element: HTMLElement;
 
     constructor() {
-        this.position = ((88 + 1) * 11 + 1) / 2;
+        this.position = fieldWidth / 2;
         this.size = 60;
 
         this.element = document.createElement("div");
         this.element.classList.add("paddle");
+
+        this.redraw();
+    }
+
+    private redraw() {
         this.element.style.left = this.position + "px";
         this.element.style.width = this.size + "px";
     }
 
+    private mouseHandler(event: MouseEvent) {
+        this.position =
+            Math.min(fieldWidth - this.size / 2,
+            Math.max(this.size / 2, event.offsetX)
+        );
+
+        this.redraw();
+    }
+
     public setup(gameElement: HTMLElement): void {
         gameElement.appendChild(this.element);
+        gameElement.addEventListener("mousemove", this.mouseHandler.bind(this));
+        gameElement.addEventListener("mouseenter", this.mouseHandler.bind(this));
+        gameElement.addEventListener("mouseleave", this.mouseHandler.bind(this));
     }
 }
---
+++ src/game/geometry.ts
@@ -0,0 +1,2 @@
+
+export const fieldWidth = (88 + 1) * 11 + 1;
